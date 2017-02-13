b7d209be5f3198fd259daeb7538739560150f1c0
pj_zhong <pj_zhong@163.com>
2017-02-13 13:44:29 +0800

    Freemarker support test done

--- jp-spring-webtest/src/main/java/com/jp/interceptors/TestInterceptor2.java
+++ jp-spring-webtest/src/main/java/com/jp/interceptors/TestInterceptor2.java
@@ -14,12 +14,12 @@ public class TestInterceptor2 implements Interceptor {
 
     @Override
     public boolean beforeHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        System.out.println("I am test2 before");
+        System.out.println("I am TestInterceptor2 before");
         return true;
     }
 
     @Override
     public void afterHandle(HttpServletRequest request, HttpServletResponse response, Object handle) {
-        System.out.println("I am test2 after");
+        System.out.println("I am TestInterceptor2 after");
     }
 }
